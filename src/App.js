e8dc8fe2cf2c8925a4c2ebe59c11c55a98d1be0d
mdsaj <mdsajjadhossain614@gmail.com>
2024-10-23 16:28:27 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React from "react";
 import './App.css';
 import Home from "./components/Home";
@@ -12,7 +11,7 @@ import Header from "./components/Header";
 
 function App() {
     return (
-        <div>
+        <div className="app-container">
             <Header/>
             <Home/>
             <About/>
@@ -20,7 +19,6 @@ function App() {
             <Project/>
             <Contact/>
             <Footer/>
-
         </div>
     );
 }
