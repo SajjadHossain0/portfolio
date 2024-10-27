bff9713c2110569a56e06bd3dcb2c6a819ca6990
mdsaj <mdsajjadhossain614@gmail.com>
2024-10-27 19:50:31 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -28,3 +28,12 @@ function App() {
 }
 
 export default App;
+
+/*
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+*/
