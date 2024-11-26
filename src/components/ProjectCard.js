ca5d0a2d737341f10ec9f2b1fb190df7d1ff0238
mdsaj <mdsajjadhossain614@gmail.com>
2024-11-26 17:40:18 +0600

    new commit

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,12 +2,19 @@ import React from "react";
 import './ProjectCard.css'
 
 function ProjectCard({title,description,link,imgLink}) {
+    const truncateText = (text, maxLength) => {
+        if (text.length > maxLength) {
+            return text.slice(0, maxLength) + '...';
+        }
+        return text;
+    };
+
     return (
         <div>
             <div className="project-card">
                 <img src={imgLink} alt={title} className="project-image"/>
                 <h3>{title}</h3>
-                <p>{description}</p>
+                <p>{truncateText(description,50)}</p>
                 <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">View
                     Project</a>
             </div>
