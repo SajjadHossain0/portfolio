c9865723002b0cb18ce4cce224a7518fe5ca9701
mdsaj <mdsajjadhossain614@gmail.com>
2025-02-18 19:40:41 +0600

    refactor(design): refactored the design css.

--- src/components/PortfolioTools.js
+++ src/components/PortfolioTools.js
@@ -11,12 +11,11 @@ export default function PortfolioTools(){
 
                 <div className="product-grid-div">
                     <div className="product-grid">
-
                         <ProjectCard
-                            imgLink="https://github.com/user-attachments/assets/0ecd6f93-0f58-489f-b041-491c444f3409"
-                            title="Bookshelf Web Application"
-                            description="A simple bookshelf web application that allows users to add, view, and manage a collection of books with cover images, titles, and author details. The app uses local storage to save the added books, ensuring the data persists across browser sessions."
-                            link="https://sajjadhossain0.github.io/Book-Collection/"
+                            imgLink="https://github.com/user-attachments/assets/4d5aef1c-277c-4119-9cad-7c55548a42a8"
+                            title="Pomodoro Timer"
+                            description="A simple Pomodoro Timer app that works on a every browser. A time management technique developed by Francesco Cirillo."
+                            link="https://sajjadhossain0.github.io/Pomodoro-Timer/"
                             linkText="Try it now"/>
                         <ProjectCard
                             imgLink="https://github.com/user-attachments/assets/8d8e296f-f36e-4e05-ba91-307410482ee4"
@@ -25,17 +24,18 @@ export default function PortfolioTools(){
                             link="https://sajjadhossain0.github.io/qr-generator/"
                             linkText="Try it now"/>
                         <ProjectCard
+                            imgLink="https://github.com/user-attachments/assets/0ecd6f93-0f58-489f-b041-491c444f3409"
+                            title="Bookshelf Web Application"
+                            description="A simple bookshelf web application that allows users to add, view, and manage a collection of books with cover images, titles, and author details. The app uses local storage to save the added books, ensuring the data persists across browser sessions."
+                            link="https://sajjadhossain0.github.io/Book-Collection/"
+                            linkText="Try it now"/>
+                        <ProjectCard
                             imgLink="https://github.com/user-attachments/assets/b6ba56bf-cefa-4a32-afd8-a7b272e0459e"
                             title="Age Calculator"
                             description="A professional and user-friendly web-based Age Calculator that calculates a user's precise age in years, months, and days based on their selected birthdate. This tool is ideal for quickly determining ages for various purposes, such as forms, documentation, or personal curiosity."
                             link="https://sajjadhossain0.github.io/Age-calculator/"
                             linkText="Try it now"/>
-                        <ProjectCard
-                            imgLink="https://github.com/user-attachments/assets/4d5aef1c-277c-4119-9cad-7c55548a42a8"
-                            title="Pomodoro Timer"
-                            description="A simple Pomodoro Timer app that works on a every browser. A time management technique developed by Francesco Cirillo."
-                            link="https://sajjadhossain0.github.io/Pomodoro-Timer/"
-                            linkText="Try it now"/>
+
 
                     </div>
                 </div>
